e1e5ac5e8eb809cde9546cdb2c09180214e650fa
milemotigo <mtigo@gmail.com>
2023-11-07 12:59:02 +0100

    task3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python3
 """ Auth class
 """
-from flask import request
+from flask import Flask, request
 from typing import List, TypeVar
 
 
 class Auth:
+    ''' A Class to manage the API authentication.
+    '''
+
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """that returns False
         - path and excluded_paths"""
         return False
 
-    def authorization_header(self, request=None) -> str:
+    def authorization_header(
+            self,
+            request=None
+            ) -> str:
         """
         returns None
         - request will be the Flask request object
         """
-        return None
+        if request is None:
+            return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """
-        returns None
-        -request will be the Flask request object
-        """
+    def current_user(
+            self,
+            request=None
+            ) -> TypeVar('User'):
+        ''' Current User
+        '''
+        request = Flask(__name__)
         return None
