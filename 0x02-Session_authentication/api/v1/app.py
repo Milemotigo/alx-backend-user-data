397b42ab74546c89479be78e356b4fab2248746b
milemotigo <mtigo@gmail.com>
2023-11-10 00:04:24 +0100

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -47,8 +47,9 @@ def before_request():
     if auth.current_user(request) is None:
         abort(403)
 
-    if auth.authorization_header(request) and auth.session_cookie(request):
-        abort(401)
+    if request.path not in excluded_paths:
+        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+            abort(401)
 
     request.current_user = auth.current_user(request)
 
