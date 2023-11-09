3024a917c2aacf4d8eb6da159c1b4c7fe8dde2ce
milemotigo <mtigo@gmail.com>
2023-11-09 19:28:28 +0100

    task2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 """session_auth"""
 from api.v1.auth.auth import Auth
-import os
+import uuid
 
 
-class SessioAuth(Auth):
+class SessionAuth(Auth):
     """SessionAuth that inherits from Auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id:
+        - Return None if user_id is None
+        - Return None if user_id is not a string
+        """ 
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
