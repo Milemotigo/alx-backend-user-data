c15dd6b580155a0845eeff17668c1d1c8eb7b4e7
milemotigo <mtigo@gmail.com>
2023-11-14 13:43:50 +0100

    new commit

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+from user import Base, User
+session = Session()
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """add a new user to the user database"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+        
+    def find_user_by(self, **kwargs):
+        """Find a user in the database by keyword arguments."""
+        try:
+            query = self._session.query(User)
+            for key, value in kwargs.items():
+                if hasattr(User, key):
+                    query = query.filter(getattr(User, key) == value)
+            return query.one()
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
