1f3ea44cc0336726f53877cd096d569dbe131f5b
milemotigo <mtigo@gmail.com>
2023-11-14 14:46:34 +0100

    new commit

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,7 +39,7 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
-        
+
     def find_user_by(self, **kwargs):
         """Find a user in the database by keyword arguments."""
         try:
