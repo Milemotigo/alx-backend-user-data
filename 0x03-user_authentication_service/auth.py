08b35d5100100fb01a45ab8385138718f1d93a36
milemotigo <mtigo@gmail.com>
2023-11-15 22:55:10 +0100

    new

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+""" basic authentication
+"""
+from user import User
+import bycrypt
+
+class Auth:    
     def _hash_password(password: str) -> str:
         """returned bytes is a salted hash of the input password"""
         
