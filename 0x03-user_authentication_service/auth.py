57b683dc72e498a3afb744e22a8f92388d399e7b
milemotigo <mtigo@gmail.com>
2023-11-16 00:34:39 +0100

    task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,8 +2,17 @@
 """ basic authentication
 """
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 import bcrypt
+
+
+def _hash_password(password: str) -> str:
+    """returned bytes is a salted hash of the input password"""
+    byte = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(byte, salt)
+    return hashed
 
 
 class Auth:
@@ -13,12 +22,20 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def _hash_password(self, password: str) -> str:
-        """returned bytes is a salted hash of the input password"""
-        byte = password.encode('utf-8')
-        salt = bcrypt.gensalt()
-        hashed = bcrypt.hashpw(byte, salt)
-        return hashed
-
+    # def register_user(self, email: str, password: str) -> User:
+    #     user = self._db.find_user_by(email=email)
+    #     if user:
+    #         raise ValueError(f"User {email} already exists")
+    #     else:
+    #         raise NoResultFound()
+    #     pw = _hash_password(password)
+    #     new_user = self._db.add_user(email, pw)
+    #     return new_user
     def register_user(self, email: str, password: str) -> User:
-        pass
+        """Adds a new user to the database.
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError(f"User {email} already exists")
