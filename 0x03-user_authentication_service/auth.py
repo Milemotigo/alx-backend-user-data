b8a87bcfa61b8a9605f722c357f75947e6c2ef82
milemotigo <mtigo@gmail.com>
2023-11-16 00:12:02 +0100

    task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,20 +6,19 @@ from db import DB
 import bcrypt
 
 
-def _hash_password(password: str) -> str:
-    """returned bytes is a salted hash of the input password"""
-    byte = password.encode('utf-8')
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(byte, salt)
-    return hashed
-
-
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
+
+    def _hash_password(self, password: str) -> str:
+        """returned bytes is a salted hash of the input password"""
+        byte = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(byte, salt)
+        return hashed
 
     def register_user(self, email: str, password: str) -> User:
         pass
