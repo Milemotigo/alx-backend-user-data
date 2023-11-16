31f495f92d4b889082e873cf5041472b69a96128
milemotigo <mtigo@gmail.com>
2023-11-16 01:56:37 +0100

    task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -19,7 +19,6 @@ def msg():
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def create_user() -> str:
     """Add a user at this endpoint"""
-    '''
     try:
         email = request.form.get("email")
         password = request.form.get("password")
@@ -27,16 +26,6 @@ def create_user() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 404
-    '''
-    try:
-        email = request.form.get("email")
-        password = request.form.get("password")
-        AUTH.register_user(email=email, password=password)
-        return jsonify({"email": email, "message": "user created"})
-    user = AUTH._db.find_user_by(email)
-    except ValueError:
-        if user:
-            return jsonify({"message": "email already registered"}), 404
 
 
 if __name__ == "__main__":
