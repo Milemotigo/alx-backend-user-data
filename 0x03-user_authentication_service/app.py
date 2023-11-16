f3bc985ebaf6f0884d0a47a657ba8cdd29404602
milemotigo <mtigo@gmail.com>
2023-11-16 01:33:22 +0100

    task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -21,17 +21,17 @@ def users() -> str:
     """add a user in this end point"""
     email = request.form.get(email)
     password = request.form.get(password)
-    #user = AUTH._db.find_user_by(email)
+    '''user = AUTH._db.find_user_by(email)
     #if user:
     #   return jsonify({"message": "email already registered"}), 404
     #AUTH.register_user(email=email, password=password)
     #return ({"email": email, "message": "user created"}), 201
+    '''
     try:
         AUTH.register_user(email=email, password=password)
         return ({"email": email, "message": "user created"}), 201
     except ValueError():
         return jsonify({"message": "email already registered"}), 404
-
 
 
 if __name__ == "__main__":
