71aee26ec8318c4458786df24b012b07fa5fe863
milemotigo <mtigo@gmail.com>
2023-11-16 19:27:19 +0100

    task 11

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,3 +68,12 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User or None:
+        """
+        returns the corresponding User or None
+        """
+        user = self._db.find_user_by(session_id=session_id)
+        if session_id is None:
+            return None
+        return user
