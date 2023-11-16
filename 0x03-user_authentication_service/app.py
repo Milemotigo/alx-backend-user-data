f549f1a19d102473c7635eceb9ee0ef0fa3584bc
milemotigo <mtigo@gmail.com>
2023-11-16 19:12:29 +0100

    task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -26,6 +26,22 @@ def create_user() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 404
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """implements the login """
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if not AUTH.valid_login(email, password):
+            abort(401)
+    except ValueError:
+        return None
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,15 +30,6 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    # def register_user(self, email: str, password: str) -> User:
-    #     user = self._db.find_user_by(email=email)
-    #     if user:
-    #         raise ValueError(f"User {email} already exists")
-    #     else:
-    #         raise NoResultFound()
-    #     pw = _hash_password(password)
-    #     new_user = self._db.add_user(email, pw)
-    #     return new_user
     def register_user(self, email: str, password: str) -> User:
         """Adds a new user to the database.
         """
