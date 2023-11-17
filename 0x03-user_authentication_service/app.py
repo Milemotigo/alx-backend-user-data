2c6d86327300badd71e65832451e2e128c0d7f91
milemotigo <mtigo@gmail.com>
2023-11-17 01:47:38 +0100

    flsk

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -44,16 +44,28 @@ def login() -> str:
     return response
 
 
-@app.route("/sessions", methods=['DELETE'], strict_slashes=False)
-def logout():
-    """Delete a session"""
-    session_id = request.cookies.get('session_id')
+# @app.route("/sessions", methods=['DELETE'], strict_slashes=False)
+# def logout():
+#     """Delete a session"""
+#     session_id = request.cookies.get('session_id')
+#     user = AUTH.get_user_from_session_id(session_id)
+#     if user:
+#         AUTH.destroy_session(user.id)
+#         return redirect('/')
+#     else:
+#         abort(403)
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """DELETE /sessions
+    Return:
+        - Redirects to home route.
+    """
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-    else:
+    if user is None:
         abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 @app.route('/profile', methods=["GET"], strict_slashes=False)
